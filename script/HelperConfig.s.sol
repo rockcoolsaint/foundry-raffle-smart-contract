9002c36ba189f535ed103b38e7154ac53f22de7d
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-07-11 14:50:47 +0100

    Link token mocks

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,6 +13,7 @@ contract HelperConfig is Script {
         bytes32 gasLane;
         uint64 subscriptionId;
         uint32 callbackGasLimit;
+        address link;
     }
 
     NetworkConfig public activeNetworkConfig;
@@ -33,7 +34,8 @@ contract HelperConfig is Script {
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 subscriptionId: 0,
-                callbackGasLimit: 500000 // 500,000 gas!
+                callbackGasLimit: 500000, // 500,000 gas!
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
             });
     }
 
